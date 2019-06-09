4bc2ef2a57b2c206f4b33175b2e1e2c235a9c51a
alexanderjsingleton <alexanderjsingleton@gmail.com>
2019-06-09 16:04:33 -0500

    installed mongoose for user-form registration data storage and testing

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes/index');
-const app = express();
 const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
-app.use('/', routes);
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/', routes);
 
 module.exports = app;
